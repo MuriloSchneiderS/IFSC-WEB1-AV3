81e2ab13df8c9c9eb6899880d21b68f63172d6e5
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2025-11-19 21:20:15 -0300

    correcoesde estilo

--- scripts/script.js
+++ scripts/script.js
@@ -2,53 +2,58 @@ document.addEventListener('DOMContentLoaded', () => {
     const apiUrl = '../backend/api.php';
     const postForm = document.getElementById('postForm');
     const listaPosts = document.getElementById('listaPosts');
-
+    
     fetchPosts();
-
+    
     postForm.addEventListener('submit', handleFormSubmit);
-
+    
     listaPosts.addEventListener('click', (event) => {
         handleFavoriteClick(event);
         handleEditClick(event);
         handleDeleteClick(event);
     });
-
+    
     // Função para carregar a lista de posts
     async function fetchPosts() {
         listaPosts.innerHTML = '<p>Carregando posts...</p>';//Mensagem padrão
-
+        
         try {
             const response = await fetch(`${apiUrl}?action=readAll`);
             if (!response.ok)
                 throw new Error('Erro ao buscar posts: ' + response.statusText);
+            
             //Armazena o resultado em uma lista de posts em formato JSON
             const posts = await response.json();
-
+            if (posts.length === 0) {
+                listaPosts.innerHTML = '<p>Nenhum post encontrado.</p>';
+                return;
+            }
+            posts.sort((a, b) => b.status - a.status);//Deve estar listado como as favoritas em primeiro lugar seguido de outros posts
+            
             listaPosts.innerHTML = '';//Limpa a mensagem padrão
             posts.forEach(post => {
                 const div = document.createElement('div');
+                div.className = 'post p-3 rounded shadow-sm bg-dark';
                 div.innerHTML = `
-                <div class="post p-3 rounded shadow-sm bg-dark">
-                    <div class="post-header d-flex justify-content-between align-items-center mb-2">
-                        <small class="text-white-50">${post.data_criacao}</small>
-                        <div>
-                            <div id="post_lista">
-                                <button name="favoritar_post" data-id="${post.id}" type="button" class="btn btn-sm btn-warning me-2">
-                                    <!-- Ícone de estrela preenchida se for favorito(bi-star-fill / bi-star) -->
-                                    <i class="bi bi-star${post.status===1?'-fill':''}" style="pointer-events: none;"></i>
-                                </button>
-                                <button name="editar_post" data-id="${post.id}" type="button" class="btn btn-sm btn-primary me-2">
-                                    <i class="bi bi-pencil" style="pointer-events: none;"></i>
-                                </button>
-                                <button name="excluir_post" data-id="${post.id}" type="button" class="btn btn-sm btn-danger">
-                                    <i class="bi bi-trash" style="pointer-events: none;"></i>
-                                </button>
-                            </div>
+                <div class="post-header d-flex justify-content-between align-items-center mb-2">
+                    <small class="text-white-50">${dataFormatoHtml(post.data_criacao)}</small>
+                    <div>
+                        <div id="post_lista">
+                            <button name="favoritar_post" data-id="${post.id}" type="button" class="btn btn-sm btn-warning me-2">
+                                <!-- Ícone de estrela preenchida se for favorito(bi-star-fill / bi-star) -->
+                                <i class="bi bi-star${post.status===1?'-fill':''}" style="pointer-events: none;"></i>
+                            </button>
+                            <button name="editar_post" data-id="${post.id}" type="button" class="btn btn-sm btn-primary me-2">
+                                <i class="bi bi-pencil" style="pointer-events: none;"></i>
+                            </button>
+                            <button name="excluir_post" data-id="${post.id}" type="button" class="btn btn-sm btn-danger">
+                                <i class="bi bi-trash" style="pointer-events: none;"></i>
+                            </button>
                         </div>
                     </div>
-                    <div class="post-body">
-                        <p name="mensagem_post" class="mb-0">${post.mensagem}</p>
-                    </div>
+                </div>
+                <div class="post-body">
+                    <p name="mensagem_post" class="mb-0">${post.mensagem}</p>
                 </div>
                 `;
                 listaPosts.appendChild(div);
@@ -58,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Erro no fetchPosts:', error);
         }
     }
-
+    
     async function handleFormSubmit(event) {
         event.preventDefault();
         
@@ -68,20 +73,20 @@ document.addEventListener('DOMContentLoaded', () => {
             postForm.querySelector('button[name="cancelar_edicao"]').style.display = 'none'; //Esconde novamente o botão de cancelar edição
             return;
         }
-
+        
         const id = postForm.dataset.id; // Verifica se há um ID armazenado para edição
         const mensagem = postForm.mensagem.value;
         const status = postForm.status ? (postForm.status.checked ? 1 : 0) : 0;
-        const data_criacao = formatDate(new Date());
-
+        const data_criacao = dataFormatoSql(new Date());
+        
         const postData = { mensagem, status, data_criacao };
         let action = 'create';
-
+        
         if (id) {
             postData.id = id;
             action = 'update';
         }
-
+        
         try {
             const response = await fetch(`${apiUrl}?action=${action}`, {
                 method: 'POST',
@@ -90,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify(postData),
             });
-
+            
             if (response.status != 204){//204 No Content para update
                 const result = await response.json();
                 if (!response.ok) {
@@ -98,27 +103,27 @@ document.addEventListener('DOMContentLoaded', () => {
                     return;
                 }
             }
-
+            
             resetForm();
             fetchPosts();
-
+            
         } catch (error) {
             console.error('Erro ao salvar post:', error);
             alert('Erro ao salvar post.');
         }
     }
-
+    
     //editar_post
     async function handleEditClick(event) {
         if (event.target.name!='editar_post')
             return;
-
+        
         const id = event.target.dataset.id;
         
         try {
             const response = await fetch(`${apiUrl}?action=readOne&id=${id}`);
             const post = await response.json();
-
+            
             postForm.mensagem.value = post.mensagem;
             postForm.dataset.id = post.id; // Armazena o ID do post no formulário
             postForm.dataset.action = 'update'; // Define a ação como 'update' para edição
@@ -127,12 +132,12 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Erro ao buscar post para edição:', error);
         }
     }
-
+    
     //excluir_post
     async function handleDeleteClick(event) {
         if (event.target.name!='excluir_post')
             return;
-
+        
         const id = event.target.dataset.id;
         
         if (!confirm('Tem certeza que deseja deletar este post?'))
@@ -160,7 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Erro ao deletar post.');
         }
     }
-
+    
     //favoritar_post
     async function handleFavoriteClick(event) {
         if (event.target.name!='favoritar_post')
@@ -180,7 +185,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify(postData),
             });
-
+            
             if (response.status != 204){//204 No Content para update
                 const result = await response.json();
                 if (!response.ok) {
@@ -194,7 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Erro ao favoritar post.');
         }
     }
-
+    
     //reset form
     function resetForm() {
         postForm.reset();
@@ -203,15 +208,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     //Função para formatar a data
-    function formatDate(date) {
-        // Format as 'YYYY-MM-DD HH:MM:SS'
+    function dataFormatoSql(date) {
+        //Formato 'YYYY-MM-DD HH:MM:SS'
         const pad = n => n < 10 ? '0' + n : n;
         return date.getFullYear() + '-' +
-            pad(date.getMonth() + 1) + '-' +
-            pad(date.getDate()) + ' ' +
-            pad(date.getHours()) + ':' +
-            pad(date.getMinutes()) + ':' +
-            pad(date.getSeconds());
+        pad(date.getMonth() + 1) + '-' +
+        pad(date.getDate()) + ' ' +
+        pad(date.getHours()) + ':' +
+        pad(date.getMinutes()) + ':' +
+        pad(date.getSeconds());
+    }
+    function dataFormatoHtml(dateString) {
+        //Formato: 'DD-MM-YYYY HH:MM:SS'
+        const date = new Date(dateString);
+        const pad = n => n < 10 ? '0' + n : n;
+        return pad(date.getDate()) + '-' +
+        pad(date.getMonth() + 1) + '-' +
+        date.getFullYear() + ' ' +
+        pad(date.getHours()) + ':' +
+        pad(date.getMinutes()) + ':' +
+        pad(date.getSeconds());
     }
 });
 
